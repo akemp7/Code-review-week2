2de5f9f2b98711b1bdcf6ccf61e9d8d3278d7d60
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-16 13:48:00 -0700

    Add images to results

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,29 @@ $(document).ready(function(){
     var experienceInput= $("select#experience").val();
     var movieInput= $("select#movie").val();
     var liveInput = $("select#live").val();
-    var pirateInput = $("pirates").val();
+    var pirateInput = $("select#pirates").val();
     $(".results").hide();
     $(".name").text(name);
 
-      if(goalsInput === "General Knowledge" && experienceInput === "Beginner" || movieInput === "Life of Brian"){
+
+      if(goalsInput === "General Knowledge" && experienceInput === "Beginner" || movieInput === "Life of Brian" || pirateInput === "ninja"){
         $("#language1").fadeToggle(1000);
         $("#language1").click(function(){
           $("p").fadeOut();
+          $("img").fadeOut();
         });
 
       } else if (goalsInput === "Statistics" || pirateInput === "pirate" || experienceInput === "Intermediate") {
         $("#language3").fadeToggle(1000);
         $("#language3").click(function(){
           $("p").fadeOut();
+          $("img").fadeOut();
         });
       } else if (goalsInput === "Develop large, complex software" && experienceInput === "Advanced" || liveInput === "Space" || pirateInput === "A scholarly bear")
         $("#language2").fadeToggle(1000);
         $("#language2").click(function(){
           $("p").fadeOut();
+          $("img").fadeOut();
         });
 
       event.preventDefault();
