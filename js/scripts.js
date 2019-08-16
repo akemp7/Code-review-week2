858b551990318e2f8b58be842cd80257692a7acb
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-16 15:06:45 -0700

    Change wording of last question and edit variable name from 'pirates' to 'ego'

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,27 @@ $(document).ready(function(){
     var experienceInput= $("select#experience").val();
     var movieInput= $("select#movie").val();
     var liveInput = $("select#live").val();
-    var pirateInput = $("select#pirates").val();
+    var egoInput = $("select#alterego").val();
 
     $(".results").hide();
     $(".name").text(name);
 
-      if (goalsInput === "Select from below" || experienceInput === "Select from below" || movieInput === "Select from below" || liveInput === "Select from below" || pirateInput === "Select from below"){
+      if (goalsInput === "Select from below" || experienceInput === "Select from below" || movieInput === "Select from below" || liveInput === "Select from below" || egoInput === "Select from below"){
         $("#no").fadeToggle();
       } else {
-        if(goalsInput === "General Knowledge" && experienceInput === "Beginner" || movieInput === "Life of Brian" || pirateInput === "ninja"){
+        if(goalsInput === "General Knowledge" || experienceInput === "Beginner" || movieInput === "Life of Brian" || egoInput === "ninja"){
           $("#language1").fadeToggle(1000);
           $("#language1").click(function(){
             $("p").fadeOut();
             $("img").fadeOut();
           });
-        } else if (goalsInput === "Statistics" || pirateInput === "pirate" || experienceInput === "Intermediate") {
+        } else if (goalsInput === "Statistics" || egoInput === "pirate" || experienceInput === "Intermediate") {
           $("#language3").fadeToggle(1000);
           $("#language3").click(function(){
             $("p").fadeOut();
             $("img").fadeOut();
           });
-        } else if (goalsInput === "Develop large, complex software" && experienceInput === "Advanced" || liveInput === "Space" || pirateInput === "A scholarly bear")
+        } else if (goalsInput === "Develop large, complex software" || experienceInput === "Advanced" || liveInput === "Space" || egoInput === "A scholarly gopher")
           $("#language2").fadeToggle(1000);
           $("#language2").click(function(){
             $("p").fadeOut();
