eaf031eea9b2fce1e7d60bd3731a064d3fc363ec
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-16 11:32:55 -0700

    Add more branching to webpage to determine results of quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
   $(".quiz").submit(function(event) {
     var name = $("input#validationServer01").val();
-    var goalsInput=$("select#goals").val();
-    var experienceInput=$("select#experience").val();
+    var goalsInput= $("select#goals").val();
+    var experienceInput= $("select#experience").val();
+    var movieInput= $("select#movie").val();
+    var liveInput = $("select#live").val();
+    var pirateInput = $("pirates").val();
     $(".results").hide();
     $(".name").text(name);
 
-    if(goalsInput === "General Learning") {
-      if(experienceInput === "Beginner") {
+      if(goalsInput === "General Knowledge" && experienceInput === "Beginner"){
+          $("#language1").show();
+        }
+
+      if (movieInput === "Life of Brian" || liveInput === "Rainforest"){
         $("#language1").show();
-      }
-    }
+      } else if (goalsInput === "Statistics" || movieInput === "Jaws" || pirateInput === "pirate") {
+        $("#language3").show();
+      } else if (goalsInput === "Develop large, complex software" && experienceInput === "Advanced" || liveInput === "Space" || pirateInput === "A scholarly bear")
+        $("#language2").show();
+
+    // if(goalsInput === "General Learning") {
+    //   if(experienceInput === "Beginner") {
+    //     $("#language1").show();
+    //   }
+    // }
       event.preventDefault();
   });
 
