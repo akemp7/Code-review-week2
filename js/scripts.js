c10b785f9993015547a34275fc315e2d21f6a8ac
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-16 10:23:23 -0700

    Add branching for programming goals and experience questions to show result

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   $(".quiz").submit(function(event) {
-
     var goalsInput=$("select#goals").val();
     var experienceInput=$("select#experience").val();
     $(".results").hide();
+    $("#validationServer01").removeClass("is-invalid");
 
-    if(goalsInput === "I need a place to do statistics!") {
-      console.log(alert("#language1"));
-
-
+    if(goalsInput === "General Learning") {
+      if(experienceInput === "Beginner") {
+        $("#language1").show();
+      }
     }
+      event.preventDefault();
   });
 
 });
