9f3b242737da97af89a63a6de3bfc959d87df915
Anita Kemp <anita.k.kemp@gmail.com>
2019-08-16 14:31:19 -0700

    Add branching layer in order to prompt user to select option from menu if they select 'select from below' option

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,32 @@ $(document).ready(function(){
     var movieInput= $("select#movie").val();
     var liveInput = $("select#live").val();
     var pirateInput = $("select#pirates").val();
+
     $(".results").hide();
     $(".name").text(name);
 
-
-      if(goalsInput === "General Knowledge" && experienceInput === "Beginner" || movieInput === "Life of Brian" || pirateInput === "ninja"){
-        $("#language1").fadeToggle(1000);
-        $("#language1").click(function(){
-          $("p").fadeOut();
-          $("img").fadeOut();
+      if (goalsInput === "Select from below" || experienceInput === "Select from below" || movieInput === "Select from below" || liveInput === "Select from below" || pirateInput === "Select from below"){
+        $("#no").fadeToggle();
+      } else {
+        if(goalsInput === "General Knowledge" && experienceInput === "Beginner" || movieInput === "Life of Brian" || pirateInput === "ninja"){
+          $("#language1").fadeToggle(1000);
+          $("#language1").click(function(){
+            $("p").fadeOut();
+            $("img").fadeOut();
+          });
+        } else if (goalsInput === "Statistics" || pirateInput === "pirate" || experienceInput === "Intermediate") {
+          $("#language3").fadeToggle(1000);
+          $("#language3").click(function(){
+            $("p").fadeOut();
+            $("img").fadeOut();
+          });
+        } else if (goalsInput === "Develop large, complex software" && experienceInput === "Advanced" || liveInput === "Space" || pirateInput === "A scholarly bear")
+          $("#language2").fadeToggle(1000);
+          $("#language2").click(function(){
+            $("p").fadeOut();
+            $("img").fadeOut();
+          });
+        }
+        event.preventDefault();
         });
-
-      } else if (goalsInput === "Statistics" || pirateInput === "pirate" || experienceInput === "Intermediate") {
-        $("#language3").fadeToggle(1000);
-        $("#language3").click(function(){
-          $("p").fadeOut();
-          $("img").fadeOut();
-        });
-      } else if (goalsInput === "Develop large, complex software" && experienceInput === "Advanced" || liveInput === "Space" || pirateInput === "A scholarly bear")
-        $("#language2").fadeToggle(1000);
-        $("#language2").click(function(){
-          $("p").fadeOut();
-          $("img").fadeOut();
-        });
-
-      event.preventDefault();
   });
-
-});
